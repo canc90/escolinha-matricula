29bd8c282c568ffe494a2a5468f5fda8cc49b779
canc90 <carlos.teccel@gmail.com>
2026-06-25 22:12:07 -0300

    initial: redirect to Cloudflare tunnel

--- backend/repositories/logRepository.js
+++
@@ -1,7 +0,0 @@
-const db = require('../database/connection');
-
-function insert(data) {
-  return db('logs').insert(data);
-}
-
-module.exports = { insert };
--- jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  testMatch: ['**/tests/**/*.test.js'],
-  forceExit: true,
-  detectOpenHandles: true,
-};
